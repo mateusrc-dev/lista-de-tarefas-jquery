6333322b43b4ec0d19bf836aa9509adb9b97569a
Mateus Carvalho <mateus.raimundo1995@gmail.com>
2024-01-03 23:00:56 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# projeto do curso engenheiro front-end EBAC com o intuito de ensinar fundamentos do jquery
+
+## jquery é um framework que ajuda o desenvolvedor a trabalhar com a árvore DOM com códigos mais simples - ajuda a selecionar elementos da DOM e manipular eles de forma mais simplificada, inclusive ajudando a adicionar animações de maneira bem simples.
+
+## a aplicação consiste em uma lista de atividades na qual podemos adicionar novas atividades inserindo em um input o nome dessas atividades e clicando em um botão "adicionar".
+
+# Deploy:
+
+✔️ https://jquery-lista-atividades-snowy-two.vercel.app/
---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+
+        const nomeDaAtividade = $("#nova-atividade").val();
+        const novaAtividade = $("<li style='display: none'></li>");
+        $(`
+            <p style='text-decoration: none'>${nomeDaAtividade}</p>
+        `).appendTo(novaAtividade);
+        $(novaAtividade).appendTo('ul');
+        $(novaAtividade).fadeIn(1000);
+        $('#nova-atividade').val("");
+    });
+
+    $('ul').on('click', 'p', function(e) {
+        
+        console.log("cheguei aqui ->", e)
+        if ($(e.currentTarget).css("text-decoration").includes("none")) {       
+            $(e.currentTarget).css("text-decoration", "line-through");
+        } else {
+            $(e.currentTarget).css("text-decoration", "none");
+        }
+    });
+})
